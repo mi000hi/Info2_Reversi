dedf96e393bcd10bc5606a19e1a603eb78870ad4
mi000hi <michaelroth@hispeed.ch>
2019-12-13 00:31:46 +0100

    .jar

--- GameCoordinator.java
+++ GameCoordinator.java
@@ -72,6 +72,8 @@ public class GameCoordinator {
 		players.put("AB_rate4allStones02", new AB_rate4allStones());
 		players.put("AB_prettyGood01", new AB_prettyGood());
 		players.put("AB_prettyGood02", new AB_prettyGood());
+		players.put("DutyCalls01", new DutyCalls());
+		players.put("DutyCalls02", new DutyCalls());
 
 		// dont let program run without commandline arguments
 		if (args.length < 5) {
